7b62d13c115ced8fd01537d3e5de7fcfeefe0339
Chaitanya Deshpande <1679159@tcs.com>
2021-06-28 16:11:03 +0530

    Initial Commit

---
+++ WYSA-ReminderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WYSA-ReminderApp
+//
+//  Created by Chaitanya Deshpande on 28/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
